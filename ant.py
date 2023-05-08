25922081758bab6275d29379cf77cc9118d220e5
Flint Rose <oliverflintrose@gmail.com>
2023-05-07 22:46:39 -0500

    Minor work into getting agents to be able to  move. Unsure where this goes but having fun.

--- ant.py
+++ ant.py
@@ -1,14 +1,41 @@
-map = [[0, 0], [0, 1]]
+import os as os
+import math as math
+import random as random
+x = 3
+y = 3
+# y first x second
 
 
-def mapTranslate(map):
-    returnString = ""
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == 1:
-                map[i][j] = 0
-            else:
-                map[i][j] = 1
+class agent():
+    def __init__(self, type, location):
+        self.type = type
+        self.location = location
+
+    def move(self):
+        x = random.randint(-1, 1)
+        y = random.randint(-1, 1)
+        self.location = [max(min(self.location[0] + y, 2), 0),
+                         max(min(self.location[1] + x, 2), 0)]
 
 
-drawMap(map)
+def agentPrint():
+    map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for i in range(0, len(agents)):
+        map[agents[i].location[0]][agents[i].location[1]] = 1
+    for i in range(0, len(map)):
+        print(map[i])
+
+
+ant = agent("ant", [0, 0])
+
+agents = [ant]
+
+agentPrint()
+
+ant.move()
+print()
+
+agentPrint()
+
+while (True):
+    mapPrint(map)
